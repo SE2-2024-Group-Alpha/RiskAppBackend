c0fa5db0223507b90c7e6da32a09d2c3c2d7e99c
botLx <alex.schnabl02@gmail.com>
2024-03-18 17:25:28 +0100

    setup

---
+++ src/main/java/se2/alpha/riskappbackend/RiskAppBackendApplication.java
@@ -0,0 +1,13 @@
+package se2.alpha.riskappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskAppBackendApplication.class, args);
+	}
+
+}
