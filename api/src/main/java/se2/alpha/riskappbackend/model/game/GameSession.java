980945180af73c49ec18a90d134485c2e84fef1d
Simon Schadenbauer <simonsc@edu.aau.at>
2024-05-27 16:07:48 +0200

    fix changed random to securerandom

--- api/src/main/java/se2/alpha/riskappbackend/model/game/GameSession.java
+++ api/src/main/java/se2/alpha/riskappbackend/model/game/GameSession.java
@@ -10,6 +10,7 @@ import se2.alpha.riskappbackend.util.GameSetupFactory;
 
 import org.springframework.web.socket.WebSocketSession;
 
+import java.security.SecureRandom;
 import java.util.*;
 
 public class GameSession {
@@ -29,7 +30,7 @@ public class GameSession {
     public GameSession(String name) {
         this.name = name;
         this.sessionId = UUID.randomUUID();
-        this.shortId = new Random().nextInt(9000) + 1000; //gets a random number between 1000 and 9999
+        this.shortId = new SecureRandom().nextInt(9000) + 1000; //gets a random number between 1000 and 9999
         this.userStates = new HashMap<>();
         this.state = GameState.LOBBY;
         users = 0;
